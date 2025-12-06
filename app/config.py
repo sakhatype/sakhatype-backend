314591c406d6819aebc836547bd90c1cfb099e3f
Timur <timur63745kul@gmail.com>
2025-12-07 01:20:38 +0900

    fixed .env

--- app/config.py
+++ app/config.py
@@ -2,11 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+
     SECRET_KEY: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    ALLOWED_ORIGINS: str
 
+    ALLOWED_ORIGINS: str
+    
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 settings = Settings()
