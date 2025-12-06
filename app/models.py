c86e3ba60b4ed24ee2ea1d0df1409ea9ec2fdc40
Timur <timur63745kul@gmail.com>
2025-12-07 01:17:34 +0900

    removed useless import

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from sqlalchemy import String, DateTime, func, ForeignKey
-from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
